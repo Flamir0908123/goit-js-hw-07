ae5fbced5c466cd7d12e5ab8718b07c02bfe69a2
Flamir0908123 <bgladki86@gmail.com>
2021-04-06 22:55:41 +0300

    end

---
+++ js/task-7.js
@@ -0,0 +1,11 @@
+const fontSizeControlRef = document.querySelector('#font-size-control')
+const textRef = document.querySelector('#text');
+
+fontSizeControlRef.value = 16;
+
+fontSizeControlRef.addEventListener('input', onInputChange);
+
+function onInputChange() {
+    const currentFontSize = fontSizeControlRef.value;
+    textRef.style.fontSize = currentFontSize + "px";
+};
